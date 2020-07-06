0452d5d2f060f0a5a3f680dffafeeca26f566d64
Daniel Maher <daniel.maher@datadoghq.com>
2020-07-06 19:05:21 +0200

    replace … and minor text edits

--- datadog-integrations-intro/scripts/init.sh
+++ datadog-integrations-intro/scripts/init.sh
@@ -1,16 +1,16 @@
 # Update apt
-echo "Updating apt…" | wall -n
+echo "Updating apt..." | wall -n
 apt update
 
 # Install virtualenv package
-echo "Installing python development packages…" | wall -n
+echo "Installing python development packages..." | wall -n
 apt install -y python3.8-venv python3.8-dev
 
 # Ensure python3 is default
-echo "Setting python3 as the default…" | wall -n
+echo "Setting python3 as the default..." | wall -n
 update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
 # Ready to go!
-echo "You're good to go!" | wall -n
+echo "You're good to go! (hit enter)" | wall -n
 
 exit 0
